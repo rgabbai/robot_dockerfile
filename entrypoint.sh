6ad2a8251086b26dd2ebca475cb6a857969daede
Rony Gabbai <97525727+rgabbai@users.noreply.github.com>
2023-09-21 13:27:09 +0300

    Update entrypoint.sh - guard Arduino file setiing

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,17 @@ source /opt/ros/foxy/setup.bash
 cd repos/robot_ws
 colcon build --symlink-install
 source ./install/setup.bash
-sudo chmod 666 /dev/Arduino_UNO
+
+
+# Enable Arduino_uno device if file exist
+FILE=/dev/Arduino_UNO
+if test -f "$FILE"; then
+    echo "$FILE exists."
+    sudo chmod 666 $FILE
+else 
+    echo "Warning: $FILE does not exist."
+fi
+
 
 # teleop_twist_Joy
 cd src/teleop_twist_joy

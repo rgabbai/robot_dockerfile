3bc7bb572f49f087106d47011397ea8efc666d1e
Rony Gabbai <97525727+rgabbai@users.noreply.github.com>
2023-11-15 14:11:08 +0200

    Update activate_robot_mpu_on.bash

--- activate_robot_mpu_on.bash
+++ activate_robot_mpu_on.bash
@@ -1,1 +1,1 @@
-sudo docker run -e ROBOT_ON=1 -it --privileged --network=host  --ipc=host  -v $PWD/my_repos:/repos  -v /dev:/dev robot
+sudo docker run -e ROBOT_ON=1 -it --rm --privileged --network=host  --ipc=host  -v $PWD/my_repos:/repos  -v /dev:/dev robot
